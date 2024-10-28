62833dff8cfaec9742f8853cac72a16112245e14
Vivixell <victorrobin56@gmail.com>
2024-10-28 13:38:06 +0100

    New changes

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useRef } from 'react'
-// import HomeIndex from "../components/home";
-import SmoothScroll from '../constants/utils/SmoothScroll';
 import HomeComponent from '@/components/Home';
 
 const Home = () => {
