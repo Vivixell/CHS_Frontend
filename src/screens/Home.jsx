0087665f076da5e765460e97126f641ff5b3051e
Vivixell <victorrobin56@gmail.com>
2024-10-28 13:22:50 +0100

    first commit

---
+++ src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from 'react'
+// import HomeIndex from "../components/home";
+import SmoothScroll from '../constants/utils/SmoothScroll';
+import HomeComponent from '@/components/Home';
+
+const Home = () => {
+  return (
+    <div>
+       <HomeComponent/>
+    </div>
+  );
+};
+
+export default Home;
